0009ffbe9caf1d81c576672b27f0ed57f2a60c2d
ateals <ateals@icloud.com>
2023-07-15 20:08:18 +0900

    #2 post main fix
    스마트폰 화면에서 메인화면 잘리는 현상 수정

--- src/components/pages/thread/PostMain.tsx
+++ src/components/pages/thread/PostMain.tsx
@@ -4,8 +4,8 @@ import { code, pre } from "@/components/posts/component/Code";
 export default ({ post }: any) => {
     const MDXComponent = useMDXComponent(post.body.code);
     return (
-        <article className="py-2 px-5 pb-5 bg-[white] dark:bg-darkBg flex justify-center">
-            <div className="prose prose-sm md:prose-lg lg:prose-xl dark:prose-invert dark:prose-p:text-darkText prose-p:mb-7 [&>p>a]:break-words">
+        <article className="w-full py-2 px-5 pb-5 bg-[white] dark:bg-darkBg flex justify-center box-border">
+            <div className="w-full prose prose-sm md:prose-lg lg:prose-xl dark:prose-invert dark:prose-p:text-darkText prose-p:mb-7 [&>p>a]:break-words">
                 <MDXComponent components={{ code, pre }} />
             </div>
         </article>
